2ca6fc8da35e1083c1e87094dc85baf492edb5e4
sonaljayawardana <it20190484@my.sliit.lk>
2023-09-25 23:30:26 +0530

    user roles added

--- core-server/src/routes/protected/CultivationMethodRoute.ts
+++ core-server/src/routes/protected/CultivationMethodRoute.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express'
 import CultivationMethodController from '../../controllers/protected/CultivationMethodController'
+import { AuthorizedUserRoles } from '../../middlewares/Authorization'
+import { UserRoles } from '../../types/enum/user/UserRoles'
 
 const cultivationMethodRoute = Router()
 
 cultivationMethodRoute.post(
     '/',
+    AuthorizedUserRoles([UserRoles.Officer, UserRoles.Farmer]),
     CultivationMethodController.saveCultivationMethodAsync
 )
 
 cultivationMethodRoute.get(
     '/list',
+    AuthorizedUserRoles([UserRoles.Officer, UserRoles.Farmer]),
     CultivationMethodController.getCultivationMethodListAsync
 )
 
--- core-server/src/routes/protected/ResearchPaperRoute.ts
+++ core-server/src/routes/protected/ResearchPaperRoute.ts
@@ -1,27 +1,44 @@
-import { Router } from 'express'
-import ResearchPaperController from '../../controllers/protected/ResearchPaperController'
+import { Router } from "express";
+import ResearchPaperController from "../../controllers/protected/ResearchPaperController";
+import { AuthorizedUserRoles } from "../../middlewares/Authorization";
+import { UserRoles } from "../../types/enum/user/UserRoles";
 
-const researchPaperRoute = Router()
+const researchPaperRoute = Router();
 
-researchPaperRoute.post('/', ResearchPaperController.saveResearchPaperAsync)
+researchPaperRoute.post(
+  "/",
+  AuthorizedUserRoles([UserRoles.Officer]),
+  ResearchPaperController.saveResearchPaperAsync
+);
 
-researchPaperRoute.put('/:id', ResearchPaperController.updateResearchPaperAsync)
+researchPaperRoute.put(
+  "/:id",
+  AuthorizedUserRoles([UserRoles.Officer]),
+  ResearchPaperController.updateResearchPaperAsync
+);
 
 researchPaperRoute.delete(
-    '/:id',
-    ResearchPaperController.deleteResearchPaperAsync
-)
+  "/:id",
+  AuthorizedUserRoles([UserRoles.Officer]),
+  ResearchPaperController.deleteResearchPaperAsync
+);
 
 researchPaperRoute.get(
-    '/list',
-    ResearchPaperController.getResearchPaperListAsync
-)
+  "/list",
+  AuthorizedUserRoles([UserRoles.Officer, UserRoles.Farmer]),
+  ResearchPaperController.getResearchPaperListAsync
+);
 
 researchPaperRoute.get(
-    '/subCategories/:id/list',
-    ResearchPaperController.getResearchPapersBySubCategoriesAsync
-)
+  "/subCategories/:id/list",
+  AuthorizedUserRoles([UserRoles.Officer, UserRoles.Farmer]),
+  ResearchPaperController.getResearchPapersBySubCategoriesAsync
+);
 
-researchPaperRoute.get('/:id', ResearchPaperController.getResearchPaperAsync)
+researchPaperRoute.get(
+  "/:id",
+  AuthorizedUserRoles([UserRoles.Officer, UserRoles.Farmer]),
+  ResearchPaperController.getResearchPaperAsync
+);
 
-export default researchPaperRoute
+export default researchPaperRoute;
