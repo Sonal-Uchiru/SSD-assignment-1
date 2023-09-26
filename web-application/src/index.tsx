fd569b6e8ba3f229b5dc5ed8621eaad0f383dd8c
sonaljayawardana <it20190484@my.sliit.lk>
2023-09-26 20:25:02 +0530

    Oauth implemented

--- web-application/src/index.tsx
+++ web-application/src/index.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import './i18n/i18n'
-import { store } from './redux/store/Store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./i18n/i18n";
+import { store } from "./redux/store/Store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </Provider>
-)
+  <Provider store={store}>
+    <GoogleOAuthProvider clientId="101073085463-qpgaj4gvvvvp1s7nb26p3igil7qqn8pd.apps.googleusercontent.com">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </GoogleOAuthProvider>
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly / CRA - vitals
-reportWebVitals()
+reportWebVitals();
