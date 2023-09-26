a484e324ce3e0287e4c4349959bd53e93f06b7f8
BamunugeDR99 <drbamunuge99@gmail.com>
2023-09-26 23:26:02 +0530

    feat: Security Fixes

--- core-server/src/app.ts
+++ core-server/src/app.ts
@@ -1,93 +1,112 @@
-import express, { Request, Response, NextFunction } from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import connectorDb from './helpers/Dbconnector'
-import * as dotenv from 'dotenv'
-import { expressjwt as jwt } from 'express-jwt'
-import morgan from 'morgan'
-import compression from 'compression'
-import swaggerUI from 'swagger-ui-express'
-import swaggerJSDoc from 'swagger-jsdoc'
-import open from 'open'
-import { optionsConfig } from './utils/swagger/config/Options'
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet, { noSniff } from "helmet";
+import connectorDb from "./helpers/Dbconnector";
+import * as dotenv from "dotenv";
+import { expressjwt as jwt } from "express-jwt";
+import morgan from "morgan";
+import compression from "compression";
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import open from "open";
+import csrf from "csrf";
+import { optionsConfig } from "./utils/swagger/config/Options";
 
 // Routers
-import unprotectedRouter from './routes/unprotected/Index'
-import authenticationRouter from './routes/authentication/Index'
-import protectedRouter from './routes/protected/Index'
+import unprotectedRouter from "./routes/unprotected/Index";
+import authenticationRouter from "./routes/authentication/Index";
+import protectedRouter from "./routes/protected/Index";
 
-const app = express()
-dotenv.config({ path: './config/.env' })
+const app = express();
+dotenv.config({ path: "./config/.env" });
 
-const dbConnectionString: string = process.env.DB_CONNECTION_STRING ?? ''
-const serverPort: string = process.env.PORT ?? ''
-const jwtSecret = process.env.JWT_SECRET ?? ''
+const dbConnectionString: string = process.env.DB_CONNECTION_STRING ?? "";
+const serverPort: string = process.env.PORT ?? "";
+const jwtSecret = process.env.JWT_SECRET ?? "";
 
-app.use(helmet())
-app.use(express.json())
-app.use(cors({
-    origin: '*'
-}))
 app.use(
-    compression({
-        level: 6,
-    })
-)
+  helmet({
+    xFrameOptions: { action: "sameorigin" }, // Anti-clickjacking Header
+    xPoweredBy: false, //  Remove "X-Powered-By" header
+    noSniff: true, // X-Content-Type-Options --> Avoids MIME sniffing
+  })
+);
+
+app.use(express.json());
+
+// Restrictive cross-domain access
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://velvety-truffle-19b644.netlify.app",
+    ],
+  })
+);
+app.use(
+  compression({
+    level: 6,
+  })
+);
 
 // swagger configuration
-const swaggerSpec = swaggerJSDoc(optionsConfig(serverPort))
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+const swaggerSpec = swaggerJSDoc(optionsConfig(serverPort));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // morgan used for logging
 // app.use(morgan("dev"));
-app.use(morgan<Request, Response>('dev'))
+app.use(morgan<Request, Response>("dev"));
 
-connectorDb(dbConnectionString)
+connectorDb(dbConnectionString);
 
 // Unprotected Router
-app.use('/', unprotectedRouter)
+app.use("/", unprotectedRouter);
 // Authentication Router
-app.use('/', authenticationRouter)
+app.use("/", authenticationRouter);
 // Protected Router
-app.use('/', jwt({ secret: jwtSecret, algorithms: ['HS256'] }), protectedRouter)
+app.use(
+  "/",
+  jwt({ secret: jwtSecret, algorithms: ["HS256"] }),
+  protectedRouter
+);
 
 // 404 response
 app.use((error: any, res: Response, next: NextFunction) => {
-    try {
-        const status = error.status || 404
-        const message = error.message || 'Resource or Url not found'
-        return res.status(status).send({
-            status,
-            message,
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+  try {
+    const status = error.status || 404;
+    const message = error.message || "Resource or Url not found";
+    return res.status(status).send({
+      status,
+      message,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // 500 (Internal server error) response
 app.use((error: any, res: Response, next: NextFunction) => {
-    try {
-        const status = error.status || 500
-        const message =
-            error.message ||
-            'There was an error while processing your request, please try again'
-        return res.status(status).send({
-            status,
-            message,
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+  try {
+    const status = error.status || 500;
+    const message =
+      error.message ||
+      "There was an error while processing your request, please try again";
+    return res.status(status).send({
+      status,
+      message,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-const port = serverPort || 8080
+const port = serverPort || 8080;
 
 app.listen(port, () => {
-    console.log(`Application started on ${port}...`)
-    console.log(`Swagger Url : http://localhost:${port}/api-docs`)
-    // Open Swagger doc in browser
-    // open(`http://localhost:${port}/api-docs`)
-})
+  console.log(`Application started on ${port}...`);
+  console.log(`Swagger Url : http://localhost:${port}/api-docs`);
+  // Open Swagger doc in browser
+  // open(`http://localhost:${port}/api-docs`)
+});
 
-export default app
+export default app;
--- web-application/src/api/UnProtectedApi.ts
+++ web-application/src/api/UnProtectedApi.ts
@@ -1,23 +1,23 @@
-import axios, { AxiosError, AxiosResponse } from 'axios'
-import ApiConstants from '../constants/ApiConstants'
+import axios, { AxiosError, AxiosResponse } from "axios";
+import ApiConstants from "../constants/ApiConstants";
 
 export const unProtectedApiAsync = (
-    method: string,
-    version: string,
-    url: string,
-    data?: JSON
+  method: string,
+  version: string,
+  url: string,
+  data?: JSON
 ) => {
-    return new Promise<AxiosResponse>((resolve, reject) => {
-        axios({
-            url: `${ApiConstants.BASE_CLOUD_API_URL}/${version}/${ApiConstants.PUBLIC_URL}/${url}`,
-            method,
-            data,
-        })
-            .then((res: AxiosResponse) => {
-                resolve(res)
-            })
-            .catch((err: AxiosError) => {
-                reject(err)
-            })
+  return new Promise<AxiosResponse>((resolve, reject) => {
+    axios({
+      url: `${ApiConstants.BASE_LOCAL_API_URL}/${version}/${ApiConstants.PUBLIC_URL}/${url}`,
+      method,
+      data,
     })
-}
+      .then((res: AxiosResponse) => {
+        resolve(res);
+      })
+      .catch((err: AxiosError) => {
+        reject(err);
+      });
+  });
+};
